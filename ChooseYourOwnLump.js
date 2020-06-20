4379cc64671c2a4e584005be6f2464939c9fdbd0
Static Variable <staticvariablejames@gmail.com>
2020-06-19 22:17:49 -0500

    ChooseYourOwnLump.js: Fix handling of the Dragon's Aura... again.

--- ChooseYourOwnLump.js
+++ ChooseYourOwnLump.js
@@ -1,17 +1,17 @@
 function predictNextLumpType(grandmapocalypseStage, dragonsCurve, realityBending, rigidelSlot, grandmaCount) {
-    let dragonAura = 1 + (dragonsCurve? 0.05 : 0) + (realityBending? 0.005 : 0);
+    let dragonAura = (dragonsCurve? 1 : 0) + (realityBending? 0.1 : 0);
 
     let ripeAge = 23 * 60*60*1000; // 23 hours
-    if (Game.Has('Stevia Caelestis')) ripeAge -= 60*1000;
-    ripeAge -= 6000 * grandmaCount;
+    if (Game.Has('Stevia Caelestis')) ripeAge -= 60*60*1000;
+    ripeAge -= 6*1000 * grandmaCount;
     ripeAge -= 20*60*1000 * rigidelSlot;
-    ripeAge /= dragonAura;
+    ripeAge /= 1 + 0.05*dragonAura;
     let autoharvestTime = Game.lumpT + ripeAge + 60*60*1000;
 
     Math.seedrandom(Game.seed+'/'+autoharvestTime);
 
     let types=['normal'];
-    let loop = randomFloor(dragonAura);
+    let loop = 1 + randomFloor(dragonAura);
     for (let i=0; i<loop; i++) {
         if (Math.random()<(Game.Has('Sucralosia Inutilis')?0.15:0.1)) types.push('bifurcated');
         if (Math.random()<3/1000) types.push('golden');
