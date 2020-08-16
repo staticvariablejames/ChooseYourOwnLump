4a0079bc2e5b79866df652c3fcbf925dd73c698d
Static Variable <staticvariablejames@gmail.com>
2020-08-16 12:01:12 -0500

    Use icons instead of text to display the predictions.

--- ChooseYourOwnLump.js
+++ ChooseYourOwnLump.js
@@ -255,23 +255,68 @@ CYOL.UI.computePredictions = function() {
     CYOL.UI.cachedPredictions = currentState.filteredPredictions(CYOL.UI.settings.targetTypes(), CYOL.UI.settings.discrepancy);
 }
 
-CYOL.UI.makeLumpIcon = function(type) {
-    let background_position = '';
-    if(type === 'normal') background_position += 'background-position: -1392px -672px;';
-    if(type === 'bifurcated') background_position += 'background-position: -1392px -720px;';
-    if(type === 'golden') background_position += 'background-position: -1344px -768px;';
-    if(type === 'meaty') background_position += 'background-position: -1392px -816px;';
-    if(type === 'caramelized') background_position += 'background-position: -1392px -1296px;';
-    return '<div class="icon" style="vertical-align: middle; margin-right: -8px;' + background_position + '""></div>';
+/* Returns a string for a <div> tag that displays the given icon. */
+CYOL.UI.makeIcon = function(icon, transparent) {
+    let transparency = '';
+    let background = '';
+    if(icon === 'lump_normal') background += 'background-position: -1392px -672px;';
+    if(icon === 'lump_bifurcated') background += 'background-position: -1392px -720px;';
+    if(icon === 'lump_golden') background += 'background-position: -1344px -768px;';
+    if(icon === 'lump_meaty') background += 'background-position: -1392px -816px;';
+    if(icon === 'lump_caramelized') background += 'background-position: -1392px -1296px;';
+    if(icon === 'aura_dragons_curve') background += 'background-position: -960px -1200px;';
+    if(icon === 'aura_reality_bending') background += 'background-position: -1536px -1200px;';
+    if(icon === 'none') background += 'background-position:48px 48px;';
+    if(transparent) transparency += 'opacity: 0.2;';
+    return '<div class="icon" style="vertical-align: middle; margin: 0 -4px;' + background + transparency + '"></div>';
+}
+
+/* Same as above but for buildings instead. */
+CYOL.UI.makeGrandmaIcon = function(type) {
+    let background = "background-image: url('img/buildings.png?v=4');";
+    if(type === 'appeased') background += 'background-position: 0px -64px;';
+    if(type === 'awoken') background += 'background-position: 0px -128px;';
+    if(type === 'displeased') background += 'background-position: -64px -128px;';
+    if(type === 'angered') background += 'background-position: -128px -128px;';
+    if(type === 'none') background += 'background-position: -64px -64px;';
+    return '<div class="icon" style="vertical-align: middle;' + background + '""></div>';
+}
+
+/* Similar as above, but builds a Rigidel with a pantheon icon instead.
+ * slot === 0 means unslotted, slot === 1 means jade slot, 2 is ruby and 3 is diamond. */
+CYOL.UI.makeRigidelIcon = function(slot) {
+    let rigidel = '<div class="icon" style="background-position:-1056px -912px;"></div>';
+    let gem_background = '';
+    if(slot === 3) gem_background += 'background-position: -1104px -720px;';
+    if(slot === 2) gem_background += 'background-position: -1128px -720px;';
+    if(slot === 1) gem_background += 'background-position: -1104px -744px;';
+    if(slot === 0) gem_background += 'background-position: -1128px -744px;';
+    let gem = '<div class="icon" style="width:24px;height:24px;' + gem_background + '"></div>';
+    return rigidel+gem;
 }
 
 CYOL.UI.customLumpTooltip = function(str, phase) {
     str += '<div class="line"></div>';
     let type = CYOL.predictNextLumpType(CYOL.UI.settings.discrepancy);
-    str += 'Predicted next lump type: ' + CYOL.UI.makeLumpIcon(type) + ' ' + type + '<br />';
+    str += 'Predicted next lump type: ' + CYOL.UI.makeIcon('lump_' + type) + ' ' + type + '<br />';
     CYOL.UI.computePredictions();
-    for(let i = 0; i < CYOL.UI.settings.predictionsToDisplay && i < CYOL.UI.cachedPredictions.length; i++)
-        str += CYOL.formatPredictionState(CYOL.UI.cachedPredictions[i], CYOL.UI.settings.discrepancy) + '<br />';
+
+    str += 'Predictions: <br />';
+    for(let i = 0; i < CYOL.UI.settings.predictionsToDisplay && i < CYOL.UI.cachedPredictions.length; i++) {
+        let prediction = CYOL.UI.cachedPredictions[i];
+        str += CYOL.UI.makeIcon('lump_' + prediction.lumpType) + ': ';
+        if(prediction.grandmaCount) {
+            str += prediction.grandmaCount + 'x';
+        }
+        if(prediction.grandmapocalypseStage === 0) str += CYOL.UI.makeGrandmaIcon('appeased');
+        if(prediction.grandmapocalypseStage === 1) str += CYOL.UI.makeGrandmaIcon('awoken');
+        if(prediction.grandmapocalypseStage === 2) str += CYOL.UI.makeGrandmaIcon('displeased');
+        if(prediction.grandmapocalypseStage === 3) str += CYOL.UI.makeGrandmaIcon('angered');
+        str += CYOL.UI.makeIcon('aura_dragons_curve', !prediction.dragon.hasDragonsCurve);
+        str += CYOL.UI.makeIcon('aura_reality_bending', !prediction.dragon.hasRealityBending);
+        str += CYOL.UI.makeRigidelIcon(prediction.rigidelSlot);
+        str += '<br />';
+    }
     return str;
 }
 
