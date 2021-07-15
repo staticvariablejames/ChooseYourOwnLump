9cb620cfec8a54ce00f69410318151175cc8b68e
Static Variable James <staticvariablejames@gmail.com>
2021-07-14 21:08:43 -0500

    Test the integration with Spiced Cookies.

--- test/all.test.ts
+++ test/all.test.ts
@@ -1,6 +1,7 @@
 import { Browser, chromium } from 'playwright';
 import { basicPredictions } from './basic-predictions';
 import { userInterface } from './user-interface';
+import { integrations } from './integrations';
 
 let browser: Browser;
 
@@ -13,3 +14,4 @@ afterAll(async () => {
 
 describe('Lump types are predicted correctly', () => basicPredictions(() => browser));
 describe('User interface displays predictions correctly', () => userInterface(() => browser));
+describe('Integrations work as intended', () => integrations(() => browser));
